de1e9f2342fa6ddae757a925705c16451f49b845
unknown <taiwohusayn5@gmail.com>
2023-04-20 03:54:17 +0100

    setup firebase and vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,25 @@
 import { createStore } from 'vuex'
 
-const store = createStore({})
+const store = createStore({
+    state: {
+        user: null
+    },
+    mutations: {
+        setUser(state, payload) {
+            state.user = payload
+            console.log('user state changed:', state.user)
+        }
+    },
+    actions: {
+        signup(context, { email, password }) {
+            console.log('signup action')
+
+            //async code
+            setTimeout(() => {
+                context.commit('setUser', { email, password })
+            }, 2000)
+        }
+    }
+})
 
 export default store
